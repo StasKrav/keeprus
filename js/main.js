8348a7c99dda751b33c5f79045a0fbc9d464774a
stas <StanKrav>
2026-06-22 09:52:15 +0500

    11

--- js/main.js
+++ js/main.js
@@ -936,21 +936,22 @@ function toggleTheme() {
 function updateThemeIcon(theme) {
     const icon = document.getElementById('themeIcon');
     if (theme === 'dark') {
-        icon.innerHTML = `
-            <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
-        `;
+    		icon.innerHTML = `
+    		            <circle cx="12" cy="12" r="5"/>
+    		            <path d="M12 1v2"/>
+    		            <path d="M12 21v2"/>
+    		            <path d="M4.22 4.22l1.42 1.42"/>
+    		            <path d="M18.36 18.36l1.42 1.42"/>
+    		            <path d="M1 12h2"/>
+    		            <path d="M21 12h2"/>
+    		            <path d="M4.22 19.78l1.42-1.42"/>
+    		            <path d="M18.36 5.64l1.42-1.42"/>
+    		        `;
+        
     } else {
         icon.innerHTML = `
-            <circle cx="12" cy="12" r="5"/>
-            <path d="M12 1v2"/>
-            <path d="M12 21v2"/>
-            <path d="M4.22 4.22l1.42 1.42"/>
-            <path d="M18.36 18.36l1.42 1.42"/>
-            <path d="M1 12h2"/>
-            <path d="M21 12h2"/>
-            <path d="M4.22 19.78l1.42-1.42"/>
-            <path d="M18.36 5.64l1.42-1.42"/>
-        `;
+                    <path d="M21 12.79A9 9 0 1 1 11.21 3 7 7 0 0 0 21 12.79z"/>
+                `;
     }
 }
 
